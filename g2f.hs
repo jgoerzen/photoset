28835171e2a04c1dd91f009beb80f7edc5917074
John Goerzen <jgoerzen@complete.org>
2007-06-14 17:44:39 -0500

    adds
    darcs-hash:20070614224439-c2a52-46939ab816a0bc8c09a52e3e20a69eb1736448ec.gz

---
+++ g2f.hs
@@ -0,0 +1,11 @@
+import Data.PhotoSet.Gallery
+import Data.PhotoSet
+import System.IO
+
+main = do
+    cfg <- readFile "../../.g2f"
+    let gurl = head (lines cfg)
+    let gr = createGR gurl
+    albums <- getAlbums gr
+    print albums
+
